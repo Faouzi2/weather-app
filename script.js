ded15e0aece747e92a0327f6cacde6198f8286f2
Faouzi Serradj <151096579+Faouzi2@users.noreply.github.com>
2024-01-12 18:18:15 +0000

    Update script.js

--- script.js
+++ script.js
@@ -15,10 +15,10 @@ document.getElementById("btn").addEventListener("click", () => {
       const humidity = document.querySelector(".weather-stat .humid");
       const windSpeed = document.querySelector(".weather-stat .wind");
 
-      temperature.innerHTML = `  ${city}  ${parseInt(json.main.temp)}°C`;
-      description.innerHTML = `  ${json.weather[0].description}`;
+      temperature.innerHTML = `${city}<br> ${parseInt(json.main.temp)}°C`;
+      description.innerHTML = `${json.weather[0].description}`;
       humidity.innerHTML = `HUMIDITY: ${json.main.humidity}%`;
-      windSpeed.innerHTML = `WIND SPEED: ${json.wind.speed}`;
+      windSpeed.innerHTML = `WIND SPEED: ${json.wind.speed} MPH`;
 
       switch (json.weather[0].main) {
         case "Clear":
@@ -28,7 +28,7 @@ document.getElementById("btn").addEventListener("click", () => {
         case "Rain":
           image.src = "pics/rain.png";
           break;
-        case "Cloud":
+        case "Clouds":
           image.src = "pics/cloud.png";
           break;
         case "Mist":
@@ -45,5 +45,48 @@ document.getElementById("btn").addEventListener("click", () => {
     .catch((error) => {
       console.error("Error fetching weather data:", error);
     });
+
+  fetch(
+    `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&appid=${APIkey}`
+  )
+    .then((response) => response.json())
+    .then((json) => {
+      for (let i = 0; i < 4; i++) {
+        const dayStat = document.querySelector(`.day${i + 1}-stat`);
+        const img = document.querySelector(`.day${i + 1}img`);
+        const temp = document.querySelector(`.day${i + 1}temp`);
+        const sky = document.querySelector(`.day${i + 1}sky`);
+
+        const date = new Date(json.list[i * 8].dt * 1000);
+        const dayOfWeek = date.toLocaleDateString("en-UK", {
+          weekday: "short",
+        });
+
+        dayStat.innerHTML = dayOfWeek;
+        img.src = getWeatherIcon(json.list[i * 8].weather[0].main);
+        temp.innerHTML = `${parseInt(json.list[i * 8].main.temp)} °C`;
+        sky.innerHTML = json.list[i * 8].weather[0].description;
+      }
+    })
+    .catch((error) => {
+      console.error("Error fetching forecast data:", error);
+    });
 });
+
+function getWeatherIcon(weatherMain) {
+  switch (weatherMain) {
+    case "Clear":
+      return "pics/clear.png";
+    case "Rain":
+      return "pics/rain.png";
+    case "Clouds":
+      return "pics/cloud.png";
+    case "Mist":
+      return "pics/mist.png";
+    case "Snow":
+      return "pics/snow.png";
+    default:
+      return "pics/cloud.png";
+  }
+}
 
